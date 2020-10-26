31830df9c7c4ffd8e648049fff1253d083f23767
Susmita <66900417+Susmit01@users.noreply.github.com>
2020-10-27 00:10:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Frequently from "./components/frequently";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Frequently />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/components/frequently.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Frequently() {
+  return <p className="heading">MOST FREQUENTLY ORDERED </p>;
+}
+export default Frequently;
